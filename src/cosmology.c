a988876b909db30395e9634e3818736fe5283d51
Oleksii Sokoliuk <104352184+osokoliuk@users.noreply.github.com>
2024-08-02 14:59:50 +0100

    Update cosmology.c

--- src/cosmology.c
+++ src/cosmology.c
@@ -1697,5 +1697,53 @@ void cosmology_struct_restore(int enabled, struct cosmology *cosmology,
   if (enabled) {
     cosmology_init_neutrino_tables(cosmology);
     cosmology_init_tables(cosmology);
+    
+    c->xs = (double *)malloc(sizeof(double)*(MAXC));
+    c->ys = (double *)malloc(sizeof(double)*(MAXC));    /* arrays of MAXC doubles */
+    size_t n = 0;                         /* count of doubles returned */
+    
+    /*Parse the cosmology kind*/
+    char cosmology_type[32] = {0};
+    char cosmology_tables_dir[256] = {0};
+    char filepath[300] = {0};
+
+    parser_get_param_string(params, "Cosmology:cosmology_type", cosmology_type);
+    parser_get_param_string(params, "Cosmology:cosmology_tables_dir", cosmology_tables_dir);
+    
+    /* Read Hubble table */
+    FILE *fp;
+
+    if (strcmp(cosmology_type, "fT")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_ft.txt");
+    } else if (strcmp(cosmology_type, "fTT")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_ftt.txt");
+    } else if (strcmp(cosmology_type, "fR")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_fr.txt");
+    } else if (strcmp(cosmology_type, "fTnu")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_ftnu.txt");
+    } else if (strcmp(cosmology_type, "fTTnu")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_fttnu.txt");
+    } else if (strcmp(cosmology_type, "fRnu")==0) {
+      strcpy(filepath,cosmology_tables_dir);
+      strcat(filepath,"hubble_table_frnu.txt");
+    } else {
+      error ("No such cosmology type exists!");
+    }
+
+    fp = fopen(filepath,"r");
+
+    if (!fp)   /* validate file open for reading */
+      error ("file open failed");
+    
+    if (!(n = coefficients (fp, c->xs, c->ys))) {   /* validate coeff pairs read */
+      error("no double values read from file.");
+    }
+    fclose (fp);
   }
 }
+
